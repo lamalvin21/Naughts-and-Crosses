7f301549fd8f5e0b6fac5d700a29c4dca7eff35b
veron-eng <veronsundaram13@gmail.com>
2023-07-28 11:20:08 +0100

    second commit

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -4,17 +4,15 @@ import java.util.Scanner;
 public class Board {
     public int board[][] = new int[3][3];
 
+    public void prinBoard(){
+        for(int row = 0; i < board.length; i++){
+            for(int column = 0; j < board.length )
+        }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Whats your name player 1?");
-        String name1 = sc.nextLine();
-        System.out.println("Whats your name player 2?");
-        String name2 = sc.nextLine();
 
-        double first_go = Math.random();
-
-        System.out.println(first_go);
 
     }
+
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Whats your name player 1?");
+        String name1 = sc.nextLine();
+        System.out.println("Whats your name player 2?");
+        String name2 = sc.nextLine();
+
+//        Random rand = new Random();
+//// Obtain a number between [0 - 49].
+//        int n = rand.nextInt(1);
+//
+//        if (n == 0){
+//
+//        }
+
+//        System.out.println(n);
 
 
 
     }
-}
