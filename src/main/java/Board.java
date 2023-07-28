a6c815f35eca557cf37203bf5a7ac9287e67b4c4
lamalvin21 <alvinlam21@gmail.com>
2023-07-28 11:41:55 +0100

    create board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,18 +1,27 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.*;
 
 public class Board {
-    public int board[][] = new int[3][3];
+    public char board[][] = new char[3][3];
 
-    public void prinBoard(){
-        for(int row = 0; i < board.length; i++){
-            for(int column = 0; j < board.length )
+    public void initialiseBoard() {
+        for (int i = 0; i< board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                board[i][j] = '-';
+            }
         }
-
-
-
     }
 
-
-
+    public void printBoard(){
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Whats your name player 1?");
-        String name1 = sc.nextLine();
-        System.out.println("Whats your name player 2?");
-        String name2 = sc.nextLine();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Whats your name player 1?");
+//        String name1 = sc.nextLine();
+//        System.out.println("Whats your name player 2?");
+//        String name2 = sc.nextLine();
 
 //        Random rand = new Random();
 //// Obtain a number between [0 - 49].
@@ -15,9 +15,9 @@ public class Main {
 //        if (n == 0){
 //
 //        }
-
 //        System.out.println(n);
-
-
-
+        Board board = new Board();
+        board.initialiseBoard();
+        board.printBoard();
     }
+}
